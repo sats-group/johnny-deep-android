f431b71f63f4a5e6991f6a5f3b11f82a0e2daec7
Sindre Moen <sindrenm@gmail.com>
2021-12-14 20:12:11 +0100

    Split up MainActivity.kt (#4)
    Move MainScreen to its own file and move both into a main package.

---
+++ app/src/main/java/com/sats/johnnydeep/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.sats.johnnydeep.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
+    setContent {
+      MainScreen()
+    }
+  }
+}
