1a87a3be31b4494267d74e9e49e35a63176ab182
Sindre Moen <sindrenm@gmail.com>
2021-12-15 11:29:44 +0100

    Enable Dagger with Hilt (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ buildscript {
 
   dependencies {
     classpath("com.android.tools.build:gradle:7.0.1")
+    classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
   }
 }
