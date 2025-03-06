193b65404f6ae1c40b8847167a18c253354d656c
Sindre Moen <sindrenm@gmail.com>
2025-03-06 21:12:08 +0100

    Bump Target and Compile SDK to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,9 +14,9 @@ android {
   defaultConfig {
     applicationId = "com.sats.johnnydeep"
 
-    compileSdk = 34
+    compileSdk = 35
     minSdk = 26
-    targetSdk = 34
+    targetSdk = 35
 
     versionCode = 1
     versionName = "0.1.0"
